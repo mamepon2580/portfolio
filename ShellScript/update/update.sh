035705f6db2d9467c848b56fdf1258d2b1df7e18
mamepon2580 <y_poso_2580@yahoo.co.jp>
2018-09-24 20:59:10 +0900

    ファイル追加

---
+++ ShellScript/update/update.sh
@@ -0,0 +1,30 @@
+#update.sh
+main(){
+  func(){
+    echo "copy $i ? y/n"
+    read w
+    if [ "$w" == "y" ]; then
+      y=$(pwd)
+      z=$(sed 's/\/home\/aki\/program//g' <<END
+$y
+END
+)
+      cp $i ~/github/portfolio$z/
+      echo "$i copy"
+    elif [ "$w" == "n" ]; then
+      echo "$i didn't"
+    else
+      echo "error"
+      func
+    fi
+  }
+  x=$(ls)
+  for i in $x
+  do
+    func
+  done
+}
+
+echo "script start"
+main
+echo "script end"
