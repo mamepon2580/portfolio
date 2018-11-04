7f7f78a4c025d28150f9fd231872575127446393
Akifumi Takahashi <y_poso_2580@yahoo.co.jp>
2018-11-05 05:23:29 +0900

    guuki.js追加

---
+++ JavaScript/guuki/guuki.js
@@ -0,0 +1,40 @@
+//guuki.js
+//D100
+//--------IO処理--------
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  console.log(
+    guuki(
+      wa(
+        makeArrInt(lines[0])
+      )
+    )
+  );
+});
+//--------IO関数--------
+//文字列を数値の配列に直す関数
+function makeArrInt(str){
+  let arrInt = str.split(' ').map(Number);
+  return(arrInt);
+}
+//--------main関数--------
+//配列の2つの要素を足し算する関数
+function wa(arrInt){
+  let int = arrInt[0] + arrInt[1];
+  return(int)
+}
+//値が奇数ならYES偶数ならNOを返す関数
+function guuki(int){
+  if(int % 2 === 1){
+    return("YES");
+  }else{
+    return("NO");
+  }
+}
